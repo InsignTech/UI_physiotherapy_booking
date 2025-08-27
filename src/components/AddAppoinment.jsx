9536b00ef2097331cf6d10040fadecbcac9e0d99
safna <fathimathu29@gmail.com>
2025-08-27 13:52:33 +0530

    changes in name

--- src/components/AddAppoinment.jsx
+++ src/components/AddAppoinment.jsx
@@ -285,8 +285,7 @@ export const AddAppointmentForm = ({
             }
             className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
             required
-            min={new Date().toISOString().split("T")[0]}
-            max={new Date().toISOString().split("T")[0]}
+            
           />
         </div>
 
